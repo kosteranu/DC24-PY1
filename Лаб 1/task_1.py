cb90fa9f55a5467b51551f2c3a623a772b09bb13
kosteranu <101293397+kosteranu@users.noreply.github.com>
2024-10-20 16:50:21 +0300

    Лаб 1

---
+++ Лаб 1/task_1.py
@@ -0,0 +1,10 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+index = 4
+sum_of_numbers = sum(numbers[:index] + numbers[index + 1:])
+len_of_numbers = len(numbers)
+mean_of_numbers = sum_of_numbers / len_of_numbers
+numbers[4] = mean_of_numbers
+
+print("Измененный список:", numbers)
